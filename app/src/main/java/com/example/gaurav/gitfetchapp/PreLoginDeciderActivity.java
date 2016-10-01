ff06378d44d3da41886db0b7f472a48e0a57b0fc
Kartik Arora <chipset95@yahoo.co.in>
2016-10-01 18:48:19 +0530

    Fix #1

--- app/src/main/java/com/example/gaurav/gitfetchapp/PreLoginDeciderActivity.java
+++ app/src/main/java/com/example/gaurav/gitfetchapp/PreLoginDeciderActivity.java
@@ -2,13 +2,13 @@ package com.example.gaurav.gitfetchapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.annotation.BinderThread;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.example.gaurav.gitfetchapp.GooglePlayServices.TrackerApplication;
@@ -29,45 +29,70 @@ public class PreLoginDeciderActivity extends AppCompatActivity {
     private Tracker mTracker;
     public static String loginName = null;
 
+    @BindView(R.id.app_name_text) TextView appNameText;
+
     @Override
     protected void onResume() {
         super.onResume();
-        mTracker.setScreenName("Screen"+TAG);
+        mTracker.setScreenName("Screen" + TAG);
         mTracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_pre_login);
-        //ButterKnife.bind(this);
-
-        Typeface tf_1 = Typeface.createFromAsset(getResources().getAssets(),"font/Roboto-Medium.ttf");
-        Typeface tf_2 = Typeface.createFromAsset(getResources().getAssets(),"font/Roboto-Light.ttf");
-        //appNameText.setTypeface(tf_1);
+        setContentView(R.layout.activity_pre_login);
+        ButterKnife.bind(this);
 
         TrackerApplication application = (TrackerApplication) getApplication();
         mTracker = application.getDefaultTracker();
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if (sharedPreferences.contains(ACCESS_TOKEN_KEY) && sharedPreferences.contains(USERNAME_KEY)) {
-            AccessToken.getInstance().setAccessToken(sharedPreferences.getString(ACCESS_TOKEN_KEY, null));
-            setLoginName(sharedPreferences.getString(USERNAME_KEY, null));
 
-            Intent intent = new Intent(this, PostLoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        } else {
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        }
+        Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+        appNameText.startAnimation(fadeIn);
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (sharedPreferences.contains(ACCESS_TOKEN_KEY) && sharedPreferences.contains(USERNAME_KEY)) {
+                            AccessToken.getInstance().setAccessToken(sharedPreferences.getString(ACCESS_TOKEN_KEY, null));
+                            setLoginName(sharedPreferences.getString(USERNAME_KEY, null));
+
+                            Intent intent = new Intent(PreLoginDeciderActivity.this, PostLoginActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        } else {
+                            Intent intent = new Intent(PreLoginDeciderActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        }
+                    }
+                },2000);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+//        Typeface tf_1 = Typeface.createFromAsset(getResources().getAssets(),"font/Roboto-Medium.ttf");
+//        Typeface tf_2 = Typeface.createFromAsset(getResources().getAssets(),"font/Roboto-Light.ttf");
+        //appNameText.setTypeface(tf_1);
     }
 
-    public static void setLoginName(String username){
+    public static void setLoginName(String username) {
         loginName = username;
     }
 
-    public static String getLoginName(){
+    public static String getLoginName() {
         return loginName;
     }
 }
